fbda8677478ef876a0c0d4929133831dd29055da
Erfan Sharafi <erfansharafi60@gmail.com>
2024-06-28 17:59:28 +0330

    dynamic meta tag

--- src/app/buy-residential/[profileId]/page.js
+++ src/app/buy-residential/[profileId]/page.js
@@ -12,3 +12,15 @@ async function ProfileDetails({ params: { profileId } }) {
 }
 
 export default ProfileDetails;
+
+export const generateMetadata = async ({ params: { profileId } }) => {
+  await connectDB();
+  const profile = await Profile.findOne({ _id: profileId });
+
+  return {
+    title: profile.title,
+    description: profile.description,
+    authors: { name: profile.realState },
+    other: { mytag: "test meta tag" },
+  };
+};
